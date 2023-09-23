a9672bfb694f90b75929464b4e131f37ec993691
Hamza Naeem <hamzaquraeshi404@gmail.com>
2023-09-23 21:20:14 +0500

    HomePage & <ProfileCard/>

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import CustomButton from "./CustomButton";
 import Loading from "./Loading";
+import ProfileCard from "./ProfileCard";
 import TextInput from "./TextInput";
 import TopBar from "./TopBar";
 
-export {TextInput, Loading, CustomButton, TopBar};
+export {TextInput, Loading, CustomButton, TopBar, ProfileCard};
