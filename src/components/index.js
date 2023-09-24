271df7056e5edbdd9a892091ba058facb26a3a4a
Hamza Naeem <hamzaquraeshi404@gmail.com>
2023-09-24 18:32:18 +0500

    <FriendsCard/>

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import CustomButton from "./CustomButton";
+import FriendsCard from "./FriendsCard";
 import Loading from "./Loading";
 import ProfileCard from "./ProfileCard";
 import TextInput from "./TextInput";
 import TopBar from "./TopBar";
 
-export {TextInput, Loading, CustomButton, TopBar, ProfileCard};
+export {TextInput, Loading, CustomButton, TopBar, ProfileCard, FriendsCard};
