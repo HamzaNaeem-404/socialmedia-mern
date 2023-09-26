9010978a5960be2712a17e24c635e08baf196bc5
Hamza Naeem <hamzaquraeshi404@gmail.com>
2023-09-26 18:39:30 +0500

    Profile Page & overflow-y-auto

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,23 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 import TopBar from '../components/TopBar';
+import ProfileCard from '../components/ProfileCard';
+import FriendsCard from '../components/FriendsCard';
+import Loading from '../components/Loading';
+import PostCard from '../components/PostCard';
+import { posts } from '../assets/data';
 
 const Profile = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const dispatch = useDispatch();
-  const {user} = useSelector((state)=> state.user);
-  const {userInfo, setUserInfo} = useState(null)
-  const {posts} = useSelector((state)=> state.posts);
+  const { user } = useSelector((state) => state.user);
+  const [userInfo, setUserInfo] = useState(user); // Fixed the useState declaration
   const [loading, setLoading] = useState(false);
+
+  const handleDelete = () => {};
+  const handleLikePost = () => {};
 
   return (
     <>
-     <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg'>
-          <TopBar />
-     </div>
-    </>
-  )
-}
+      <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden'>
+        <TopBar />
+        <div className='w-full flex gap-2 lg:gap-4 md:pl-4 pt-5 pb-10 h-full'>
+          {/* LEFT */}
+          <div className='hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6 overflow-y-auto'>
+            <ProfileCard user={user} />
 
-export default Profile
+            <div className='block lg:hidden'>
+              <FriendsCard friends={userInfo?.friends} />
+            </div>
+          </div>
+
+          {/* CENTER */}
+          <div className='flex-1 h-full px-4 flex flex-col gap-6 overflow-y-auto rounded-lg'>
+            {loading ? (
+              <Loading />
+            ) : posts?.length > 0 ? (
+              posts?.map((post) => (
+                <PostCard
+                  key={post.id} // Add a key prop for each PostCard
+                  post={post}
+                  user={user}
+                  deletePost={handleDelete}
+                  likePost={handleLikePost}
+                />
+              ))
+            ) : (
+              <div className='flex w-full h-full items-center justify-center'>
+                <p className='text-lg ext-ascent-2'>No Post Available</p>
+              </div>
+            )}
+          </div>
+
+          {/* RIGHT */}
+          <div className='hidden w-1/4 h-full lg:flex flex-col gap-8 overflow-y-auto'>
+            <FriendsCard friends={user?.friends} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
